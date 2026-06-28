c88944295906c29b600ea75e3c28ea89c19b7a7f
Jazmin Iloso <matminisace15@gmail.com>
2026-06-29 02:55:02 +0800

    feat: add authentication middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import { supabase } from '../config/supabaseClient.js'; 
+
+export const requireAuth = async (req, res, next) => {
+  try {
+    // extract the authorization header
+    const authHeader = req.headers.authorization; 
+
+    // check if the header exists an starts with "Bearer"
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'Authentication token required'});
+    }
+
+    // isolate the actual token string 
+    const token = authHeader.split(' '[1]); 
+
+    // ask supabase to validate the token 
+    const { data: {user}, error } = await supabase.auth.getUser(token);
+
+    if (error || !user) {
+      return res.status(401).json({ message: 'Invalid or expired session token'});
+    }
+
+    // attach the verified user object directly to the request object 
+    req.user = user; 
+
+    next(); // move to the next controller function safely
+
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal auth server error', error: error.message })
+  }
+}
